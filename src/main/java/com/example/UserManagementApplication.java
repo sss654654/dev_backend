07068143307e599c0a7bca1e1d35f9d3c180528e
root <root@DESKTOP-8RV8P8N.localdomain>
2025-08-18 17:45:22 +0900

    edit redis files

--- src/main/java/com/example/UserManagementApplication.java
+++ src/main/java/com/example/UserManagementApplication.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.example.usermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example") 
 @EnableJpaAuditing
 public class UserManagementApplication {
     public static void main(String[] args) {
