cfd841818a0638517c3fcd7f383c4fc62f9464ec
Sehi55 <sehi1028@naver.com>
2025-08-19 16:40:00 +0900

    comment

--- src/main/java/com/example/couponmanagement/controller/CouponController.java
+++ src/main/java/com/example/couponmanagement/controller/CouponController.java
@@ -30,49 +30,49 @@ public class CouponController {
     private final AdmissionService admissionService;
 
     @Autowired
-    public CouponController(CouponService couponService,AdmissionService admissionService,SessionService sessionService) {
+    public CouponController(CouponService couponService, AdmissionService admissionService, SessionService sessionService) {
         this.couponService = couponService;
         this.admissionService = admissionService;
         this.sessionService = sessionService;
     }
 
-    @PostMapping("/request")
-    public ResponseEntity claimAny(@RequestBody RequestCoupon req,
-                                                       HttpServletRequest request) {
-        String requestId = couponService.acceptAny(req.getRequestId());
-//        String s3Url = "https://your-s3-bucket-name.s3.ap-northeast-2.amazonaws.com/wait.html?requestId=" + requestId;
+//    @PostMapping("/request")
+//    public ResponseEntity claimAny(@RequestBody RequestCoupon req,
+//                                                       HttpServletRequest request) {
+//        String requestId = couponService.acceptAny(req.getRequestId());
+////        String s3Url = "https://your-s3-bucket-name.s3.ap-northeast-2.amazonaws.com/wait.html?requestId=" + requestId;
+////
+////        // S3 URL로 직접 리다이렉트하라고 브라우저에 명령
+////        return ResponseEntity
+////                .status(303)
+////                .location(URI.create(s3Url))
+////                .build();
+//        String waitPath = "/wait.html?requestId=" + requestId;
 //
-//        // S3 URL로 직접 리다이렉트하라고 브라우저에 명령
-//        return ResponseEntity
-//                .status(303)
-//                .location(URI.create(s3Url))
-//                .build();
-        String waitPath = "/wait.html?requestId=" + requestId;
+//
+//        String sessionId = sessionService.readSessionIdFromCookie(request)
+//                .orElseThrow(() -> new ResponseStatusException(UNAUTHORIZED, "NO_SESSION_COOKIE"));
+//
+//
+//        if (!sessionService.isSessionValid(sessionId)) {
+//            throw new ResponseStatusException(HttpStatusCode.valueOf(419), "SESSION_EXPIRED");
+//        }
+//        AdmissionService.AdmissionResult result = admissionService.tryEnter(requestId);
+//
+//        if (result == AdmissionService.AdmissionResult.SUCCESS) {
+//            return ResponseEntity.ok("즉시 입장 처리되었습니다. 예매 페이지로 이동합니다.");
+//        } else {
+//            //QUEUED
+//             return ResponseEntity
+//                    .accepted()                               // 202 Accepted (비동기 큐에 올림)
+//                    .header(HttpHeaders.LOCATION, waitPath)   // 선택: Location도 실어줌
+//                    .body(Map.of(
+//                            "myseq", requestId,
+//                            "waitUrl", waitPath
+//                    ));
+//        }
 
 
-        String sessionId = sessionService.readSessionIdFromCookie(request)
-                .orElseThrow(() -> new ResponseStatusException(UNAUTHORIZED, "NO_SESSION_COOKIE"));
 
-
-        if (!sessionService.isSessionValid(sessionId)) {
-            throw new ResponseStatusException(HttpStatusCode.valueOf(419), "SESSION_EXPIRED");
-        }
-        AdmissionService.AdmissionResult result = admissionService.tryEnter(sessionId);
-
-        if (result == AdmissionService.AdmissionResult.SUCCESS) {
-            return ResponseEntity.ok("즉시 입장 처리되었습니다. 예매 페이지로 이동합니다.");
-        } else {
-            //QUEUED
-             return ResponseEntity
-                    .accepted()                               // 202 Accepted (비동기 큐에 올림)
-                    .header(HttpHeaders.LOCATION, waitPath)   // 선택: Location도 실어줌
-                    .body(Map.of(
-                            "myseq", requestId,
-                            "waitUrl", waitPath
-                    ));
-        }
-
-
-    }
 
 }
